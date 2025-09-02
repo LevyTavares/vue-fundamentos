46c1488ee879f2740f4930a4ecaf422db2441c08
LevyTavares <isaiaslevi2@gmail.com>
2025-09-01 21:04:10 -0300

    feat: initialize Vue project with Vite, add product catalog layout and styles
    - Created package.json with dependencies and scripts for Vite and Vue.
    - Added favicon.ico for the application.
    - Implemented main application structure in App.vue with a product form and ProductCard component.
    - Added base CSS for styling and color palette.
    - Included logo.svg for branding.
    - Created main.css for application layout styling.
    - Developed ProductCard.vue component to display product information.
    - Set up main.js to bootstrap the Vue application.
    - Configured Vite with vue and vue-devtools plugins in vite.config.js.

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+  import ProductCard from './components/ProductCard.vue';
+</script>
+
+<template>
+<main>
+  <h1>Catálogo de Produtos</h1>
+
+  <form>
+    <div>
+      <label for="nome">Nome</label>
+      <input id="nome" type="text" required/>
+    </div>
+    <div>
+      <label for="descricao">Descrição</label>
+      <input id="descricao" type="text" required/>
+    </div>
+    
+    <div>
+      <label for="preco">Preço</label>
+      <input id="preco" type="text" required/>
+    </div>
+    <div>
+      <label for="imagem">Imagem URL</label>
+      <input id="imagem" type="text" required/>
+    </div>
+    <div>
+      <button type="submit">Adicionar Produto</button>
+    </div>
+  </form>
+
+  <ProductCard/>
+</main>
+</template>
+
+<style scoped>
+  form {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+  }
+  form label{
+    display: block;
+    margin-bottom: .2rem;
+  }
+  form input {
+    box-sizing: border-box;
+    padding: .5rem;
+    width: 100%;
+  }
+  button {
+    padding: .5rem;
+    background-color: #007BFF;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    max-width: 200px;
+    margin-left: auto;
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="product-card">
+    <img
+      src="https://www.sabornamesa.com.br/media/k2/items/cache/7f23dcfbe64e4348bd21e29b1ec76fb1_XL.jpg"
+      alt="Imagem do Produto"
+      width="100%"
+      height="180px"
+    />
+    <h3>Sorvete de Flocos</h3>
+    <p>Sorvete de Flocos, caseiro</p>
+    <span>R$ 29.00</span>
+  </div>
+</template>
+
+<style scoped>
+.product-card {
+  border: solid 1px #fff;
+  padding: 1rem;
+  border-radius: 8px;
+  text-align: center;
+}
+.product-card h3 {
+    color:#fff;
+    font-weight: 600;
+}
+.product-card span{
+    color: green;
+}
+</style>
